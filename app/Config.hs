ef88d3d14c22a3e2d4c3248269c936b192d0640a
David Langen <david.langen@klosebrothers.de>
2021-02-17 20:38:03 +0100

    add Config file

---
+++ app/Config.hs
@@ -0,0 +1,29 @@
+module Config where
+import Graphics.Gloss.Data.Color
+
+-- Basic Configs
+amountOfCells :: Int
+amountOfCells = 10
+
+screenWidth :: Int
+screenWidth = 500
+
+screenHeight :: Int
+screenHeight = 500
+
+cellWidth :: Float
+cellWidth = fromIntegral screenWidth / fromIntegral amountOfCells
+
+cellHeight :: Float
+cellHeight = fromIntegral screenHeight / fromIntegral amountOfCells
+
+-- Design / Color Configs
+
+backgroundColor :: Color
+backgroundColor = makeColor 0 0 0 255
+
+gridColor :: Color
+gridColor = makeColor 150 150 150 255
+
+gameOverTextColor :: Color
+gameOverTextColor = makeColor 150 150 150 255
