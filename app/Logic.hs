91773a9e4bd18973f97f828e9c8fb452f96074fb
David Langen <david.langen@klosebrothers.de>
2021-02-19 19:34:51 +0100

    check tail collision

--- app/Logic.hs
+++ app/Logic.hs
@@ -46,24 +46,27 @@ getOppositeDirectionOf d = case d of
 checkOppositeDirection :: Direction -> Direction -> Bool
 checkOppositeDirection a b = a == getOppositeDirectionOf b
 
-checkWallCollison :: Cell -> Bool
-checkWallCollison (x, y) = x >= amountOfCells || y >= amountOfCells || x < 0 || y < 0
+checkWallAndTailCollison :: Snake -> Bool
+checkWallAndTailCollison s = checkWallHeadCollison (head s) || head s `elem` tail s
+
+checkWallHeadCollison :: Cell -> Bool
+checkWallHeadCollison (x,y) = x >= amountOfCells || y >= amountOfCells || x < 0 || y < 0
 
 -- vielleicht mit record matching
 translateSnakeHeadWithSpeed :: Direction -> Int -> Game -> Game
 translateSnakeHeadWithSpeed d speed game
-  | checkWallCollison $ head newSnakeHead = game {gameState = GameOver}
-  | otherwise = updateSnakeAndDirection game newSnakeHead d
+  | checkWallAndTailCollison newSnake = game {gameState = GameOver}
+  | otherwise = updateSnakeAndDirection game newSnake d
   where
     sn = snake (gamePlayer game)
     withLastElem = head sn `elem` (gameBoard game)
-    newSnakeHead = updateSnakeHead withLastElem (vectorByDirection d speed) sn
+    newSnake = updateSnake withLastElem (vectorByDirection d speed) sn
 
-updateSnakeHead :: Bool -> (Int, Int) -> Snake -> Snake
-updateSnakeHead _ _ [] = []
-updateSnakeHead _ (amountX, amountY) [(x, y)] = [(amountX + x, amountY + y)]
-updateSnakeHead withLastElem (amountX, amountY) (h@(x, y) : xs) = (amountX + x, amountY + y) : h : take (length xs - wipedLastedElem) xs
-                                                    where wipedLastedElem = if withLastElem then 0 else 1 
+updateSnake :: Bool -> (Int, Int) -> Snake -> Snake
+updateSnake _ _ [] = []
+updateSnake _ (amountX, amountY) [(x, y)] = [(amountX + x, amountY + y)]
+updateSnake withLastElem (amountX, amountY) (h@(x, y) : xs) = (amountX + x, amountY + y) : h : take (length xs - amountOfLatestElem) xs
+                                                    where amountOfLatestElem = if withLastElem then 0 else 1
 
 updateSnakeAndDirection :: Game -> Snake -> Direction -> Game
 updateSnakeAndDirection game s d = game {gamePlayer = (gamePlayer game) {snake = s, direction = d}}
