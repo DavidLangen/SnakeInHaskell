b9396e6b5f739f0668092e07d3daca41f029c13f
David Langen <david.langen@klosebrothers.de>
2021-02-19 18:17:57 +0100

    ignore input in opposite Direction

--- app/Logic.hs
+++ app/Logic.hs
@@ -7,10 +7,10 @@ transformGame = handleKeys
 
 
 handleKeys :: Event -> Game -> Game
-handleKeys (EventKey (Char 'w') Down _ _) game = translateSnakeHead UP game
-handleKeys (EventKey (Char 'a') Down _ _) game = translateSnakeHead LEFT game
-handleKeys (EventKey (Char 'd') Down _ _) game = translateSnakeHead RIGHT game
-handleKeys (EventKey (Char 's') Down _ _) game = translateSnakeHead DOWN game
+handleKeys (EventKey (Char 'w') Down _ _) game = moveSnake UP game
+handleKeys (EventKey (Char 'a') Down _ _) game = moveSnake LEFT game
+handleKeys (EventKey (Char 'd') Down _ _) game = moveSnake RIGHT game
+handleKeys (EventKey (Char 's') Down _ _) game = moveSnake DOWN game
 handleKeys _ game = game
 
 updateOverTime :: Float -> Game -> Game
@@ -24,18 +24,32 @@ vectorByDirection d speed = case d of
                       DOWN ->  (-speed, 0)
                       LEFT ->  (0, -speed)
                       RIGHT -> (0, speed)
-translateSnakeHead :: Direction -> Game -> Game
-translateSnakeHead d = translateSnakeHeadWithSpeed d 1
+
+moveSnake :: Direction -> Game -> Game
+moveSnake d game@Game{gamePlayer=Player{direction=playerD}} 
+           | checkOppositeDirection d playerD = game
+           | otherwise = translateSnakeHeadWithSpeed d 1 game 
+
+getOppositeDirectionOf :: Direction -> Direction
+getOppositeDirectionOf d = case d of
+                             UP -> DOWN
+                             DOWN -> UP
+                             LEFT -> RIGHT
+                             RIGHT -> LEFT
+
+checkOppositeDirection :: Direction -> Direction -> Bool
+checkOppositeDirection a b = a == getOppositeDirectionOf b
 
 checkWallCollison :: Cell -> Bool
 checkWallCollison (x,y) = x >= amountOfCells || y >= amountOfCells || x < 0 || y < 0
 
 -- vielleicht mit record matching
 translateSnakeHeadWithSpeed :: Direction -> Int -> Game -> Game
-translateSnakeHeadWithSpeed d speed game = if checkWallCollison $ head newSnakeHead then game{gameState = GameOver}
-                                           else updateSnakeAndDirection game newSnakeHead d
-                                           where sn = snake (gamePlayer game)
-                                                 newSnakeHead = updateSnakeHead (vectorByDirection d speed) sn
+translateSnakeHeadWithSpeed d speed game
+                                  | checkWallCollison $ head newSnakeHead =  game{gameState = GameOver}
+                                  | otherwise = updateSnakeAndDirection game newSnakeHead d
+                                  where sn = snake (gamePlayer game)
+                                        newSnakeHead = updateSnakeHead (vectorByDirection d speed) sn
 
 updateSnakeHead :: (Int, Int) -> Snake -> Snake
 updateSnakeHead _ [] = []
