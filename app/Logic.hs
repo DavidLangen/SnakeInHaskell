b0def08ef1d2bbddf59f6eaf49fbfbe7831b39de
David Langen <david.langen@klosebrothers.de>
2021-02-19 18:21:06 +0100

    reformat

--- app/Logic.hs
+++ app/Logic.hs
@@ -1,10 +1,11 @@
 module Logic where
-import Game
+
 import Config
+import Game
 import Graphics.Gloss.Interface.Pure.Game
+
 transformGame :: Event -> Game -> Game
 transformGame = handleKeys
-
 
 handleKeys :: Event -> Game -> Game
 handleKeys (EventKey (Char 'w') Down _ _) game = moveSnake UP game
@@ -15,50 +16,48 @@ handleKeys _ game = game
 
 updateOverTime :: Float -> Game -> Game
 updateOverTime time game = translateSnakeHeadWithSpeed directionOfPlayer speed game
-              where speed = truncate (snakeSpeed + time)
-                    directionOfPlayer = direction (gamePlayer game)
-                    
+  where
+    speed = truncate (snakeSpeed + time)
+    directionOfPlayer = direction (gamePlayer game)
+
 vectorByDirection :: Direction -> Int -> (Int, Int)
 vectorByDirection d speed = case d of
-                      UP   ->  (speed , 0)
-                      DOWN ->  (-speed, 0)
-                      LEFT ->  (0, -speed)
-                      RIGHT -> (0, speed)
+  UP -> (speed, 0)
+  DOWN -> (- speed, 0)
+  LEFT -> (0, - speed)
+  RIGHT -> (0, speed)
 
 moveSnake :: Direction -> Game -> Game
-moveSnake d game@Game{gamePlayer=Player{direction=playerD}} 
-           | checkOppositeDirection d playerD = game
-           | otherwise = translateSnakeHeadWithSpeed d 1 game 
+moveSnake d game@Game {gamePlayer = Player {direction = playerD}}
+  | checkOppositeDirection d playerD = game
+  | otherwise = translateSnakeHeadWithSpeed d 1 game
 
 getOppositeDirectionOf :: Direction -> Direction
 getOppositeDirectionOf d = case d of
-                             UP -> DOWN
-                             DOWN -> UP
-                             LEFT -> RIGHT
-                             RIGHT -> LEFT
+  UP -> DOWN
+  DOWN -> UP
+  LEFT -> RIGHT
+  RIGHT -> LEFT
 
 checkOppositeDirection :: Direction -> Direction -> Bool
 checkOppositeDirection a b = a == getOppositeDirectionOf b
 
 checkWallCollison :: Cell -> Bool
-checkWallCollison (x,y) = x >= amountOfCells || y >= amountOfCells || x < 0 || y < 0
+checkWallCollison (x, y) = x >= amountOfCells || y >= amountOfCells || x < 0 || y < 0
 
 -- vielleicht mit record matching
 translateSnakeHeadWithSpeed :: Direction -> Int -> Game -> Game
 translateSnakeHeadWithSpeed d speed game
-                                  | checkWallCollison $ head newSnakeHead =  game{gameState = GameOver}
-                                  | otherwise = updateSnakeAndDirection game newSnakeHead d
-                                  where sn = snake (gamePlayer game)
-                                        newSnakeHead = updateSnakeHead (vectorByDirection d speed) sn
+  | checkWallCollison $ head newSnakeHead = game {gameState = GameOver}
+  | otherwise = updateSnakeAndDirection game newSnakeHead d
+  where
+    sn = snake (gamePlayer game)
+    newSnakeHead = updateSnakeHead (vectorByDirection d speed) sn
 
 updateSnakeHead :: (Int, Int) -> Snake -> Snake
 updateSnakeHead _ [] = []
-updateSnakeHead (amountX, amountY) [(x,y)] = [(amountX + x, amountY + y)]
-updateSnakeHead (amountX, amountY) (h@(x,y):xs) = (amountX + x, amountY + y) : h : take (length xs - 1) xs
-
+updateSnakeHead (amountX, amountY) [(x, y)] = [(amountX + x, amountY + y)]
+updateSnakeHead (amountX, amountY) (h@(x, y) : xs) = (amountX + x, amountY + y) : h : take (length xs - 1) xs
 
 updateSnakeAndDirection :: Game -> Snake -> Direction -> Game
 updateSnakeAndDirection game s d = game {gamePlayer = (gamePlayer game) {snake = s, direction = d}}
-
-
-
